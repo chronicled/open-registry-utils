d0a524ed77df115204447cae61d24746a37ce6af
AugustoL <augusto.lemble@gmail.com>
2016-07-29 15:15:22 -0300

    Added bytes32Array methods, keypair method generation finished, tests updated

---
+++ README.md
@@ -0,0 +1,1 @@
+# Chronicled Utils Lib
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 module.exports = {
-    ec: require('./lib/ec'),
-    urn: require('./lib/urn.js'),
-    keyPair: require('./lib/keyPair.js')
+  ec: require('./lib/ec'),
+  urn: require('./lib/urn.js'),
+  keyPair: require('./lib/keyPair.js'),
+  bytes32Array: require('./lib/bytes32Array.js')
 }
