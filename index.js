feee1176e4110e869e5ce1d55ce63f24e9bbe5ea
Oleksii Matiiasevych <lastperson@gmail.com>
2016-08-08 19:10:29 +0300

    Remove implicit base64 from urn.create, add common.compressAll

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ module.exports = {
   rsa: require('./lib/rsa'),
   urn: require('./lib/urn'),
   keyPair: require('./lib/keyPair'),
-  bytes32Array: require('./lib/bytes32Array')
+  bytes32Array: require('./lib/bytes32Array'),
+  common: require('./lib/common')
 }
